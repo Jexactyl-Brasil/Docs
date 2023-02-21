8f04477b9896e29b82e3dcdd052cd9dcf8296bef
Drylian <109999325+drylian@users.noreply.github.com>
2023-02-21 00:53:27 -0300

    teste

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('C:\\Users\\Drylian\\Documents\\GitHub\\Docs\\node_modules\\infima\\dist\\css\\default\\default.css'),
+  require('C:\\Users\\Drylian\\Documents\\GitHub\\Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
+  require('C:\\Users\\Drylian\\Documents\\GitHub\\Docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
+  require('C:\\Users\\Drylian\\Documents\\GitHub\\Docs\\src\\css\\custom.css'),
+];
